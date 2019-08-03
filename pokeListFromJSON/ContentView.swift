ea1f402d8f03386966cb5ae86fc8f870935c5ee1
Roger Price <rwprwp@gmail.com>
2019-08-03 22:30:57 +0800

    Initial Commit

---
+++ pokeListFromJSON/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  pokeListFromJSON
+//
+//  Created by bel mount on 3/8/2019.
+//  Copyright © 2019 bel mount. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
